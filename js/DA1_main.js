5e7b3a5bf7e1787eba93d680e0e0333cdc04064c
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-02 18:14:35 -0500

    sdfg

--- js/DA1_main.js
+++ js/DA1_main.js
@@ -16,7 +16,7 @@ window.onload = function() {
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        game.load.image( 'dog', 'assets/dog_sprite_sheetx2.gif' );
+        game.load.image( 'dog', 'assets/dog_sprite_sheetx2.gif0000' );
     }
     
     var playersprite;
