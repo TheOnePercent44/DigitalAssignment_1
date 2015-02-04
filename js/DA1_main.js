b266dd2336e6761cd6cf1b1e71085d36998bdddc
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-03 20:39:24 -0500

    wdefd

--- js/DA1_main.js
+++ js/DA1_main.js
@@ -26,7 +26,7 @@ window.onload = function() {
     
     function create() {
 		map = game.add.tilemap('map');//, 32, 32);
-		map.addTilesetImage('tiles');//'More Grass?', 'tiles');
+		map.addTilesetImage('More Grass?', 'tiles', 32, 32);
 		layer = map.createLayer('Tile Layer 1');
 		//layer.resizeWorld();
         // Create a sprite at the center of the screen using the 'dog' image.
