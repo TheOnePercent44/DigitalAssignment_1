2d0543bc1fae769ed2dd29fbc14a32a792057914
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-04 22:11:54 -0500

    overcharge jumping

--- js/DA1.2_main.js
+++ js/DA1.2_main.js
@@ -80,7 +80,7 @@ Follower.prototype.update = function() {
 	
 	if(isJumping === true)
 	{
-		if((game.time.now-jumpStart) > 3000)
+		if((game.time.now-jumpStart) > 80000)
 		{
 			console.log("Jump is done");//debug
 			isJumping = false;
